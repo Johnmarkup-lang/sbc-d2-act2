fd8c02615ffd0c6694ca8b5861e667ed9d580a8b
JOHNMARK <hoomanthe3rd@gmail.com>
2024-06-19 04:02:59 -0700

    Add files via upload

---
+++ sbc-d2-a2.py
@@ -0,0 +1,26 @@
+from random import randint
+#humpyang game
+
+options = {0: "kulob", 1: "hayang"}
+
+p1 = None
+c1 = options[randint(0, 1)]
+c2 = options[randint(0, 1)]
+
+p1 = input("pick 0 kung kulob 1 kung hayang (kulob, hayang): ")
+
+p1 = options[int(p1)]
+
+print(f"P1: {p1}")
+print(f"c1: {c1}")
+print(f"c2: {c2}")
+
+
+if p1 == c1 == c2:
+    print("parehas tanan! utro!")
+elif p1 != c1 and p1 != c2:
+    print("p1 ang daog!")
+elif c1 != p1 and c1 != c2:
+    print("c1 ang daog!")
+elif c2 != p1 and c2 != c1: 
+    print("c2 ang daog!")
